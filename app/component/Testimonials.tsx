d023ee8442e86dac6193b6c37c80f775cc9ad772
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-09-05 17:11:30 +0530

    testimonials
    Co-Authored-By: 8m4an <34647817+imranalam04@users.noreply.github.com>

---
+++ app/component/Testimonials.tsx
@@ -0,0 +1,112 @@
+"use client";
+import React from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Testimonials = () => {
+  const testimonials = [
+    {
+      id: 1,
+      name: "Maria Smantha",
+      image: "https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp",
+      text: "Lorem ipsum dolor sit amet eos adipisci, consectetur adipisicing elit.",
+    },
+    {
+      id: 2,
+      name: "Lisa Cudrow",
+      image: "https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp",
+      text: "Neque cupiditate assumenda in maiores repudi mollitia architecto.",
+    },
+    {
+      id: 3,
+      name: "John Smith",
+      image: "https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp",
+      text: "Delectus impedit saepe officiis ab aliquam repellat rem unde ducimus.",
+    },
+    {
+      id: 3,
+      name: "John Smith",
+      image: "https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp",
+      text: "Delectus impedit saepe officiis ab aliquam repellat rem unde ducimus.",
+    },
+  ];
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    slidesToShow: 3,
+    autoplaySpeed: 1500, // Show only one slide on small screens
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768, // Breakpoint for tablets
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024, // Breakpoint for desktops
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="overflow-hidden">
+      <section className="bg-black h-screen">
+        <div className="mx-auto md:text-center  md:max-w-xl lg:max-w-3xl">
+          <h3 className="mb-6 text-5xl font-bold md:mx-0 text-white">
+            Testimonials
+          </h3>
+          <p className="mb-6 pb-2 md:mb-12 md:pb-0 mx-1 text-white ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+            error amet numquam iure provident voluptate esse quasi, veritatis
+            totam voluptas nostrum quisquam eum porro a pariatur veniam.
+          </p>
+        </div>
+        <Slider {...settings}>
+          {testimonials.map((testimonial) => (
+            <div key={testimonial.id} className="text-center">
+              <div className="md:mx-3 mx-0  block rounded-lg bg-white shadow-lg dark:shadow-black/30">
+                <div className="h-28 overflow-hidden rounded-t-lg bg-[#9d789b]"></div>
+                <div className="mx-auto -mt-12 w-24 overflow-hidden rounded-full border-2 border-white bg-white">
+                  <img
+                    src={testimonial.image}
+                    className="w-full"
+                    alt={testimonial.name}
+                  />
+                </div>
+                <div className="p-6 my-5 ">
+                  <h4 className="mb-4 text-2xl font-semibold">
+                    {testimonial.name}
+                  </h4>
+                  <hr />
+                  <p className="mt-4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      className="inline-block h-7 w-7 pr-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+                    </svg>
+                    {testimonial.text}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </section>
+    </div>
+  );
+};
+
+export default Testimonials;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Header from "./component/Header";
 import Hero from "./component/Hero";
+import Testimonials from "./component/Testimonials";
 import { ChatAiWidget } from "@sendbird/chat-ai-widget";
 import "@sendbird/chat-ai-widget/dist/style.css";
 
@@ -9,6 +10,7 @@ export default function Home() {
     <div>
       <Header />
       <Hero />
+      <Testimonials />
       <ChatAiWidget
         applicationId="A93694F4-EAA9-436C-A442-AD43F444AD13"
         botId="onboarding_bot"
