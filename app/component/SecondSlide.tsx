bc486f6c8232d7094ace08dadee8561e0923cb14
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-09-05 17:19:04 +0530

    About us

---
+++ app/component/SecondSlide.tsx
@@ -0,0 +1,90 @@
+import { Plus, SquareCode, SquareDashedMousePointer, Tangent } from "lucide-react";
+
+const SecondSlide = () => {
+  return (
+    <div className="h-screen flex relative bg-primary text-white p-24">
+      <div className="right-dotted-line absolute top-0 h-screen left-8 w-0 border-dashed border-l-2 border-gray-800"></div>
+      <div className="top-dotted-line absolute top-8 w-full left-0 h-0 border-dashed border-b-2 border-gray-800"></div>
+      <div className="absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2">
+        <div className="inner-circle w-48 h-48 rounded-full border-dashed border-2 border-gray-800 "></div>
+      </div>
+      <div className="absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2">
+        <div className="outer-circle w-80 h-80 rounded-full border-dashed border-2 border-gray-800 "></div>
+      </div>
+      <div className="absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2 rotate-45">
+        <div className="left-diagonal w-96 border-dashed border-b-2 border-gray-800 "></div>
+      </div>
+      <div className="absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2 -rotate-45">
+        <div className="left-diagonal w-96 border-dashed border-b-2 border-gray-800 "></div>
+      </div>
+      <Plus className="absolute top-4 left-4 text-gray-600" size={40} />
+      <Plus className="absolute bottom-4 left-4 text-gray-600" size={40} />
+      <div className="flex-1 z-50">
+        <p className="text-gray-500 text-sm font-bold mb-6">
+          VISUALIZING SUCCESS
+        </p>
+        <h1 className="bg-gradient-to-r from-white to-transparent bg-clip-text text-transparent text-6xl font-bold mb-10">
+          What's in Minimal?
+        </h1>
+
+        <div className="flex flex-col gap-8 mb-10">
+          <div className="flex gap-8 bg-gray-800/25 p-6 rounded-lg shadow-lg w-3/5">
+            <div className="flex items-center mb-4">
+              <Tangent size={30} />
+            </div>
+            <div>
+              <h2 className="text-xl font-semibold">Branding</h2>
+              <p className="font-thin text-gray-300">
+                Consistent design makes it easy to brand your own.
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-8 bg-gray-800/25 p-6 rounded-lg shadow-lg w-3/5">
+            <div className="flex items-center mb-4">
+              <SquareDashedMousePointer size={30} />
+            </div>
+            <div>
+              <h2 className="text-xl font-semibold">UI & UX design</h2>
+              <p className="font-thin text-gray-300">
+                The kit is built on the principles of the atomic design system.
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-8 bg-gray-800/25 p-6 rounded-lg shadow-lg w-3/5">
+            <div className="flex items-center mb-4">
+              <SquareCode size={30} />
+            </div>
+            <div>
+              <h2 className="text-xl font-semibold">Development</h2>
+              <p className="font-thin text-gray-300">
+                Easy to customize and extend, saving you time and money.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 p-6 rounded-lg shadow-lg w-full md:w-2/3">
+        {/* <h2 className="text-xl font-semibold mb-4">
+          Area Installed (+43%) than last year
+        </h2>
+        <div className="flex justify-between mb-2">
+          <span>Asia</span>
+          <span>Europe</span>
+          <span>Americas</span>
+        </div>
+        <div className="h-64">
+          <svg className="w-full h-full" viewBox="0 0 100 100">
+            <rect x="10" y="30" width="10" height="40" fill="gray" />
+            <rect x="30" y="50" width="10" height="20" fill="gray" />
+            <rect x="50" y="10" width="10" height="80" fill="gray" />
+            <rect x="70" y="20" width="10" height="60" fill="gray" />
+            <rect x="90" y="40" width="10" height="40" fill="gray" />
+          </svg>
+        </div> */}
+        <img className="absolute z-50 border-2 border-gray-800 rounded-lg -right-0 top-20" src="/chart.jpg" />
+      </div>
+    </div>
+  );
+};
+
+export default SecondSlide;
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Hero from "./component/Hero";
 import Testimonials from "./component/Testimonials";
 import { ChatAiWidget } from "@sendbird/chat-ai-widget";
 import "@sendbird/chat-ai-widget/dist/style.css";
+import SecondSlide from "./component/SecondSlide";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Testimonials />
+      <SecondSlide />
       <Footer />
       <ChatAiWidget
         applicationId="A93694F4-EAA9-436C-A442-AD43F444AD13"
