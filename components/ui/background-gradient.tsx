ed204e373679f9d551f2d0086a77d31120a33410
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-09-07 19:43:02 +0530

    fix border on pricing

--- components/ui/background-gradient.tsx
+++ components/ui/background-gradient.tsx
@@ -59,9 +59,10 @@ export const BackgroundGradient = ({
         }
         style={{
           backgroundSize: animate ? "400% 400%" : undefined,
+          borderRadius: "0.5rem",
         }}
         className={cn(
-          "absolute inset-0 rounded-3xl z-[1] will-change-transform",
+          "absolute inset-0  z-[1] will-change-transform",
           "bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
         )}
       />
