2a6cacacccf343db0dc8db1267801755d007868f
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2024-09-15 20:44:00 +0530

    Redesigned the 2nd component

--- app/component/WhyUs.tsx
+++ app/component/WhyUs.tsx
@@ -18,66 +18,85 @@ export default function WhyUs() {
   }, [controls, inView]);
 
   return (
-    <div className="dark:bg-black py-16 px-4 sm:px-6 lg:px-8">
+    <div className="dark:bg-black py-5 px-4 ml-28 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         <motion.h2
           initial={{ x: "-10%", opacity: 0 }}
           animate={controls}
           ref={ref}
-          className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-10 text-center bg-gradient-to-r from-black dark:from-white to-transparent bg-clip-text"
+          className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-10 text-start bg-gradient-to-r from-black dark:from-white to-transparent bg-clip-text"
         >
-          Make time for what matters,
-          <br className="hidden sm:inline" />
-          let AI handle the hardwork
+          <div className="flex">
+            <div>
+              <p className="text-8xl text-slate-300 dark:text-gray-500">05</p>
+            </div>
+            <div className="p-4 py-2">
+              <p className="text-green-500 text-lg">KEY METRICS</p>
+              What did we discover initially?
+            </div>
+          </div>
         </motion.h2>
+
+        <p className="text font-semibold">
+          In order to understand the main issues and needs of the users,we
+          conducted user interview sessions among
+          <br />
+          carefully selected recruitment company employees.
+        </p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
           {/* First Stat */}
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={controls}
-            className="flex flex-col items-center text-center space-y-2"
+            className="flex flex-col items-center text-center mt-32 space-y-2"
           >
-            <h3 className="text-5xl sm:text-6xl font-bold text-green-500">75%</h3>
-            <p className="text-lg sm:text-xl font-semibold mt-4">
-              Operations shift from
-              <br />
-              production to strategy
+            <h3 className="text-5xl sm:text-9xl font-bold text-green-500">
+              81%
+            </h3>
+            <p className="text-lg font-semibold mt-4">
+              find it hard to manage incoming resumes.
             </p>
-            <span className="text-sm text-gray-400 mt-2">Source: Gartner</span>
           </motion.div>
 
           {/* Second Stat */}
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={controls}
-            className="flex flex-col items-center text-center space-y-2"
+            className="flex flex-col items-center text-start  mt-32 space-y-2"
           >
-            <h3 className="text-5xl sm:text-6xl font-bold text-green-500">59%</h3>
-            <p className="text-lg sm:text-xl font-semibold mt-4">
-              Productivity
+            <h3 className="text-5xl sm:text-9xl font-bold text-green-500">
+              53%
+            </h3>
+            <p className="text-lg font-semibold mt-4">
+              would love to colaborate more with
               <br />
-              improvement
+              other recruiters in the recruitment process.
             </p>
-            <span className="text-sm text-gray-400 mt-2">Source: NNGroup, MIT</span>
           </motion.div>
 
           {/* Third Stat */}
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={controls}
-            className="flex flex-col items-center text-center space-y-2"
+            className="flex flex-col items-center text-start  mt-32 space-y-2"
           >
-            <h3 className="text-5xl sm:text-6xl font-bold text-green-500">40%</h3>
-            <p className="text-lg sm:text-xl font-semibold mt-4">
-              Higher-quality task
+            <h3 className="text-5xl sm:text-9xl font-bold text-green-500">
+              71%
+            </h3>
+            <p className="text-lg font-semibold mt-4">
+              sees room for recruitment process
               <br />
-              output
+              improvement.
             </p>
-            <span className="text-sm text-gray-400 mt-2">Source: Harvard</span>
           </motion.div>
         </div>
+        <p className="font-semibold pt-28 text-start">
+          To learn more we carried out the survey among an even broader focus
+          group.Below we present some of the questions with the results.
+        </p>
       </div>
     </div>
   );
 }
+
