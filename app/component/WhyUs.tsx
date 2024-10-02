11d4d7dc5c6f06f4f89e01d2fefdcd4b6af21079
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2024-10-02 15:25:59 +0530

    basic ui for survey

--- app/component/WhyUs.tsx
+++ app/component/WhyUs.tsx
@@ -27,7 +27,7 @@ const WhyUs: React.FC = () => {
     percentage,
     children,
   }) => {
-    const fillPercentage = percentage - 10
+    const fillPercentage = percentage - 10;
 
     return (
       <div className="flex flex-col items-center mt-12 space-y-2">
@@ -128,7 +128,13 @@ const WhyUs: React.FC = () => {
         We did survey, across india for this product, in the year 2023 - 2024
       </p>
       <div className="flex font-semibold  pb-4 mx-0 sm:mx-16">
-        <p className="text-[12px]">*To learn more about this <a href="mailto:compliance@springreen.in" className="text-purple-500">click here</a> and learn more about suvey contact us compliance@springreen.in</p>
+        <p className="text-[12px]">
+          *To learn more about this{" "}
+          <Link href="/form" className="text-purple-500">
+            click here
+          </Link>{" "}
+          and learn more about suvey contact us compliance@springreen.in
+        </p>
       </div>
     </div>
   );
